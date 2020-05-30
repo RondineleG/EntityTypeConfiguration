c1002698455e0709e14b56cbd6810746f69a7970
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-05-30 16:40:49 -0300

    Adicionando configuração as entidades

---
+++ src/EntityTypeConfiguration/EntityConfig/LIvroConfig.cs
@@ -0,0 +1,26 @@
+﻿using EntityTypeConfiguration.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityTypeConfiguration.EntityConfig
+{
+    public class LIvroConfig : IEntityTypeConfiguration<Livro>
+    {
+        public void Configure(EntityTypeBuilder<Livro> livro)
+        {
+            livro.ToTable("Livro");
+
+            livro.HasKey(x => x.LivroId);
+
+            livro.Property(x => x.Titulo)
+                .HasColumnType("varchar(200)");
+
+            livro.Property(x => x.Autor)
+                .HasColumnType("varchar(100)");
+
+            livro.Property(x => x.DataCadastro)
+                .HasColumnType("date");
+
+        }
+    }
+}
---
+++ src/EntityTypeConfiguration/EntityConfig/LivroDetalhesConfig.cs
@@ -0,0 +1,26 @@
+﻿using EntityTypeConfiguration.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityTypeConfiguration.EntityConfig
+{
+    public class LivroDetalhesConfig : IEntityTypeConfiguration<LivroDetalhes>
+    {
+        public void Configure(EntityTypeBuilder<LivroDetalhes> livroDetalhe)
+        {
+            livroDetalhe.ToTable("LivroDetalhe");
+
+            livroDetalhe.HasKey(x => x.LivroId);
+
+            livroDetalhe.Property(x => x.Editora)
+                .HasColumnType("varchar(100)");
+
+            livroDetalhe.Property(x => x.NumeroDePaginas)
+                .HasColumnType("smallint");
+
+            livroDetalhe.HasOne(x => x.Livro)
+                .WithOne(x => x.Detalhes);
+
+        }
+    }
+}
