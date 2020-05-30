e7cc2772853350da9a0100da1eb3538c0f77ade9
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-05-30 16:40:29 -0300

    Criando as entidades

---
+++ src/EntityTypeConfiguration/Entities/LivroDetalhes.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityTypeConfiguration.Entities
+{
+    public class LivroDetalhes
+    {
+        public int LivroId { get; set; }
+
+        public string Editora { get; set; }
+
+        public int NumeroDePaginas { get; set; }
+
+        public virtual Livro Livro { get; set; }
+    }
+}
