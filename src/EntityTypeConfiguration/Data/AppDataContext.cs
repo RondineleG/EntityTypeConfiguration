35bd2ed180e1d244d2cc55f7b56741913395d833
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-05-30 16:40:13 -0300

    Criando e configurando o acesso ao banco e mapeamento das entidades

---
+++ src/EntityTypeConfiguration/Data/AppDataContext.cs
@@ -0,0 +1,23 @@
+﻿using EntityTypeConfiguration.Entities;
+using EntityTypeConfiguration.EntityConfig;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityTypeConfiguration.Data
+{
+    public class AppDataContext : DbContext
+    {
+
+        public DbSet<Livro> Livro { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCore;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new LIvroConfig());
+            modelBuilder.ApplyConfiguration(new LivroDetalhesConfig());
+        }
+
+    }
+}
